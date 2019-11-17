5bd71b0667ebfdbed1fce7fc426aaa592d99d099
aharburg <connect@aaronharburg.com>
2019-11-16 20:48:52 -0800

    Styling and component breakdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import HomePage from './homepage.component.js'
+import HomePage from './pages/homepage/homepage.component.js'
 
 function App() {
   return <div>
-    <HomePage></HomePage>
+    <HomePage />
   </div>;
 }
 

5b1fc9f02c60e24a7c8621c2f75410a39cc09393
aharburg <connect@aaronharburg.com>
2019-11-27 14:57:19 -0800

    Added the toggle car image.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import userReduecer from "./user/user.reducer";
+import cartReducer from './cart/cart.reducer'
 
 export default combineReducers({
-  user: userReduecer
+  user: userReduecer,
+  cart: cartReducer
 });

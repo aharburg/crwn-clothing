fa152983d773d5e3af5f27a0eb51b6cb199fada9
aharburg <connect@aaronharburg.com>
2019-12-13 12:47:42 -0800

    Collections update and changed to object.

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import { addItem } from "../../redux/cart/cart.actions";
 
-import "./collection-item.styles.scss";
+import './collection-item.styles.scss'
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;

f8f18533a492d5d70b3f402e0e15fa8528cb0b3d
aharburg <connect@aaronharburg.com>
2019-12-13 12:02:20 -0800

    Updated the collections components.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import MenuItem from "../menu-item/menu-item.component";
+import MenuItem  from "../menu-item/menu-item.component";
 import { selectDirectorySections } from "../../redux/directory/directory.selectors";
 
 import "./directory.styles.scss";
 
 const Directory = ({ sections }) => (
   <div className="directory-menu">
-    {this.state.sections.map(({ id, ...otherSectionProps }) => (
+    {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  selections: selectDirectorySections
+  sections: selectDirectorySections
 });
 
 export default connect(mapStateToProps)(Directory);

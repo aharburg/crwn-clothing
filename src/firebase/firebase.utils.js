97e0ace3401bf998d0ff3cc3b5e2d078c6ba4e40
aharburg <connect@aaronharburg.com>
2019-12-13 17:38:01 -0800

    Updated signin components to fix signout problems.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -33,7 +33,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
@@ -44,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;

bdca2b627c8ffdbf5af7bc393ae3572e7ef78e0a
aharburg <connect@aaronharburg.com>
2020-01-04 17:19:12 -0800

    Updated with Sagas

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -33,7 +33,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
@@ -45,7 +45,6 @@ export const addCollectionAndDocuments = async (
   objectsToAdd
 ) => {
   const collectionRef = firestore.collection(collectionKey);
-  console.log(collectionRef);
 
   const batch = firestore.batch();
   objectsToAdd.forEach(obj => {
@@ -57,7 +56,7 @@ export const addCollectionAndDocuments = async (
 };
 
 export const convertCollectionsSnapshotToMap = collections => {
-  const transformedCollections = collections.docs.map(doc => {
+  const transformedCollection = collections.docs.map(doc => {
     const { title, items } = doc.data();
 
     return {
@@ -67,17 +66,27 @@ export const convertCollectionsSnapshotToMap = collections => {
       items
     };
   });
-  return transformedCollections.reduce((accumulator, collection) => {
+
+  return transformedCollection.reduce((accumulator, collection) => {
     accumulator[collection.title.toLowerCase()] = collection;
     return accumulator;
   }, {});
 };
 
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,27 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-//Libraries
+import createSagaMiddleWare from "redux-saga";
 
 import rootReducer from "./root-reducer";
+import rootSaga from './root-saga'
 
-const middlewares = [];
+const sagaMiddleWare = createSagaMiddleWare();
+
+const middlewares = [sagaMiddleWare];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleWare.run(rootSaga);
 
 export const persistor = persistStore(store);
 
 export default { store, persistor };
 
-if (process.env.NODE_ENV === 'development') {
-    middlewares.push(logger);
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
